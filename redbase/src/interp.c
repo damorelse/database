a8e858142b711e66c5630cd546c5795e611f66e3
Gina Pai <ginapai@gmail.com>
2015-06-07 07:41:36 -0700

    get to compile

--- redbase/src/interp.c
+++ redbase/src/interp.c
@@ -394,17 +394,17 @@ static int mk_conditions(NODE *list, int max, Condition conditions[])
          return E_TOOMANY;
 
       current = list -> u.LIST.curr;
-      conditions[i].lhsAttr.relName = 
-         current->u.CONDITION.lhsRelattr->u.RELATTR.relname;
-      conditions[i].lhsAttr.attrName = 
-         current->u.CONDITION.lhsRelattr->u.RELATTR.attrname;
+      strcpy(conditions[i].lhsAttr.relName,  
+         current->u.CONDITION.lhsRelattr->u.RELATTR.relname);
+      strcpy(conditions[i].lhsAttr.attrName, 
+         current->u.CONDITION.lhsRelattr->u.RELATTR.attrname);
       conditions[i].op = current->u.CONDITION.op;
       if (current->u.CONDITION.rhsRelattr) {
          conditions[i].bRhsIsAttr = TRUE;
-         conditions[i].rhsAttr.relName = 
-            current->u.CONDITION.rhsRelattr->u.RELATTR.relname;
-         conditions[i].rhsAttr.attrName = 
-            current->u.CONDITION.rhsRelattr->u.RELATTR.attrname;
+         strcpy(conditions[i].rhsAttr.relName,
+            current->u.CONDITION.rhsRelattr->u.RELATTR.relname);
+         strcpy(conditions[i].rhsAttr.attrName,
+            current->u.CONDITION.rhsRelattr->u.RELATTR.attrname);
       }
       else {
          conditions[i].bRhsIsAttr = FALSE;
